368f8f06e8def0d6ad0521756ad5ea04528b9b93
nodzperez <146012330+nodzperez@users.noreply.github.com>
2024-12-18 20:25:45 -0600

    Formatting and validation changes.

--- src/pages/auth/mutation.ts
+++ src/pages/auth/mutation.ts
@@ -1,16 +1,16 @@
 export const mutationLogin = async () => {
-    const response = await fetch(
-        "https://api.themoviedb.org/3/authentication/guest_session/new",
-        {
-            headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTM0ZmIxMTlmNzFhOTA3YWY3MWE5MGE0OTAyM2UxMCIsIm5iZiI6MTczNDU1MDc3OS4xMDMwMDAyLCJzdWIiOiI2NzYzMjRmYjA2OTdiNmM2YzFhYWViZWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZftoHbhbdcyrb2H95_4D7fhnZBcUdqDyAmbnAflfD9Y'    
-            },
-        }
-    );
-    
-const data  = await response.json()
-console.log(data);
+  const response = await fetch(
+    "https://api.themoviedb.org/3/authentication/guest_session/new",
+    {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTM0ZmIxMTlmNzFhOTA3YWY3MWE5MGE0OTAyM2UxMCIsIm5iZiI6MTczNDU1MDc3OS4xMDMwMDAyLCJzdWIiOiI2NzYzMjRmYjA2OTdiNmM2YzFhYWViZWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZftoHbhbdcyrb2H95_4D7fhnZBcUdqDyAmbnAflfD9Y",
+      },
+    }
+  );
 
-return data;
+  const data = await response.json();
+  console.log(data);
 
+  return data;
 };
